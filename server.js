09e1975fb7f62c4bd3f52a0b4eac6b6643462413
Teddy Johnson <tjohn13@email.cpcc.edu>
2019-02-11 17:23:32 -0500

    Update to server.js

--- server.js
+++ server.js
@@ -12,14 +12,25 @@ app.use(express.static("public"));
 app.get("/",function(req,res){
     res.send("Hello");
 
+})
+
+app.get("/s",function(req,res){
+
     axios.get("https://otakumode.com/news/label/JoJo%27s%20Bizarre%20Adventure").then(function(response){
         
     var $ = cheerio.load(response.data);
 
     var results = []
 
-    
+    $("article.p-article-list__item").each(function(i,element){
+        var link = $(element).children("a").attr("href")
+        results.push(link);
     })
+    console.log(results)
+    })
+    
+
+    res.send("Check Console Log")
 })
 
 app.listen(3000, function(){
